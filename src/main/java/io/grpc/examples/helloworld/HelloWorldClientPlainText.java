8f7316bfa2f8928f90570499d0e20e97eed79252
nassar <assarn@objectcomputing.com>
2023-04-18 09:23:35 -0500

    plan text channel working for grpc

---
+++ src/main/java/io/grpc/examples/helloworld/HelloWorldClientPlainText.java
@@ -0,0 +1,30 @@
+package io.grpc.examples.helloworld;
+
+import io.grpc.Channel;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import io.grpc.StatusRuntimeException;
+
+import java.util.logging.Level;
+
+public class HelloWorldClientPlainText {
+
+    private final GreeterGrpc.GreeterBlockingStub blockingStub;
+
+    public HelloWorldClientPlainText(Channel channel) {
+        blockingStub = GreeterGrpc.newBlockingStub(channel);
+    }
+
+    public void greet(String name) {
+        HelloRequest request = HelloRequest.newBuilder().setName(name).build();
+        HelloReply response = blockingStub.sayHello(request);
+        System.out.println("Greeting: " + response.getMessage());
+    }
+
+    public static void main(String[] args) {
+        int port = 50051;
+        ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", port).usePlaintext().build();
+        HelloWorldClientPlainText client = new HelloWorldClientPlainText(channel);
+        client.greet("Nirav Plain Text");
+    }
+}
